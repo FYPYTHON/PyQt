8ec66ed0076ac1d3da7c8b835e06d7b3f86c19e1
feiying <1823218990@qq.com>
2021-04-12 15:46:46 +0800

    update ud

--- FSTornado/shell/getud.sh
+++ FSTornado/shell/getud.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-mkdir -p /opt/log/fs
-PYTHONPATH=/opt/midware/FSTornado/python3_fs/lib/python3.5/site-packages /opt/midware/python3.5/bin/python3 /opt/midware/FSTornado/get_updown.py $1>> /opt/log/fs/ud.log 2>&1
+mkdir -p /opt/log/fs/ud
+curdate=`date +"%m%d_%Y"`
+PYTHONPATH=/opt/midware/FSTornado/python3_fs/lib/python3.5/site-packages /opt/midware/python3.5/bin/python3 /opt/midware/FSTornado/get_updown.py $1>> /opt/log/fs/ud.${curdate}.log 2>&1
