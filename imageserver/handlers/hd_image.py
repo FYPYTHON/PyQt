b04914595bb31b7d8b8d28e20cfd08a1b0ea34e3
feiying <1823218990@qq.com>
2021-07-01 16:30:05 +0800

    imageserver support mutil data

--- imageserver/handlers/hd_image.py
+++ imageserver/handlers/hd_image.py
@@ -11,13 +11,28 @@ class ImageHandler(tornado.web.RequestHandler):
     def get(self):
         xdata = self.get_arguments("xdata")
         ydata = self.get_arguments("ydata")
-        if len(xdata) != len(ydata):
+        sma1 = self.get_arguments("sma1")
+        sma2 = self.get_arguments("sma2")
+        pos = self.get_arguments("position")
+        
+        imagelog.info("{}".format(len(ydata)))
+        mutli = False
+        if pos:
+              mutli = True
+        if len(xdata) != len(ydata) and not mutli:
             return self.write(json.dumps({"err_code": 1, "msg": "x y length not the same"}))
         # print(f"{xdata} {ydata}")
         # print(xdata)
         # print(ydata)
         ydata = [float(i) for i in ydata]
-        plt.plot(xdata, ydata, marker='o')
+        if mutli:
+            sma1 = [float(i) for i in sma1]
+            sma2 = [float(i) for i in sma2]
+            pos = [int(i) for i in pos]
+            sma_data = [ydata, sma1, sma2, pos]
+            plt.plot(xdata, *sma_data, marker='o')
+        else:
+            plt.plot(xdata, ydata, marker='o')
         plt.rcParams['font.sans-serif'] = ['WenQuanYi Zen Hei Mono']
         plt.gcf().subplots_adjust(bottom=0.2)
         plt.xticks(rotation=75, fontsize=12)
